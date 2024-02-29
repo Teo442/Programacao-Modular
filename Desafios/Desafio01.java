4e8e53e935786c85c571d3c2778e476045766206
Pedro Teodoro <102976793+Teo442@users.noreply.github.com>
2024-02-29 08:17:11 -0300

    Update and rename Desafio.java to Desafio01.java

--- Desafios/Desafio01.java
+++ Desafios/Desafio01.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
 
-public class Desafio {
+public class Desafio01 {
     public static void main(String args[]) {
         int op; 
         boolean ativo = true; 
